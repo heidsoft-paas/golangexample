336fdc3f43d66227f265f05a8606fd40081a7117
hustcat <hustcat@gmail.com>
2014-12-18 15:07:41 +0800

    update

--- docker/docker_restart_for_k8s.go
+++ docker/docker_restart_for_k8s.go
@@ -7,30 +7,34 @@ import (
 	"os"
 )
 
-//ref https://github.com/fsouza/go-dockerclient
+// Restart container for kubernetes
+// When RestartPolicy is "never", network container will be created newly.
+// Add Pod's container to network container's network namespace.
+// Ref https://github.com/fsouza/go-dockerclient
 
 func main() {
 	var(
-        ID string
-        NetContainer string
+        id string
+        netContainer string
     )
-	flag.StringVar(&ID, "id", "", "container id")
-    flag.StringVar(&NetContainer, "net", "", "network container id")
+	flag.StringVar(&id, "id", "", "container id")
+    flag.StringVar(&netContainer, "net", "", "network container id")
 
 	flag.Parse()
 
-    if ID == "" || NetContainer == ""{
+    if id == "" || netContainer == ""{
         fmt.Println("container id or network container id is null")
         os.Exit(1)
     }
 	endpoint := "unix:///var/run/docker.sock"
 	client, _ := docker.NewClient(endpoint)
 
-	err := client.StartContainer(ID, &docker.HostConfig{
-		NetworkMode:  "container:"+NetContainer,
+	err := client.StartContainer(id, &docker.HostConfig{
+		NetworkMode:  "container:"+netContainer,
 	})
 	if err != nil {
 		fmt.Println("failed to start container, %v", err)
 		os.Exit(1)
 	}
+	fmt.Printf("start container %s successful", id)
 }
